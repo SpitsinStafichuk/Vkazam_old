2eadb90af863768510d4bfd4af8126dfe4aad056
Mikhail Spitsin <Mikhail.Spitsin@sidenis.com>
2013-11-07 16:51:09 +0400

    added button pres visualizer

--- gen/com/git/programmerr47/testhflbjcrhjggkth/R.java
+++ gen/com/git/programmerr47/testhflbjcrhjggkth/R.java
@@ -23,8 +23,10 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int lastfm_icon=0x7f020001;
-        public static final int micro_image_button_press=0x7f020002;
-        public static final int microphone_icon=0x7f020003;
+        public static final int micro_image_button_background=0x7f020002;
+        public static final int micro_image_button_press=0x7f020003;
+        public static final int micro_image_button_unpress=0x7f020004;
+        public static final int microphone_icon=0x7f020005;
     }
     public static final class id {
         public static final int lastFmLoginLogoutButton=0x7f08000c;
