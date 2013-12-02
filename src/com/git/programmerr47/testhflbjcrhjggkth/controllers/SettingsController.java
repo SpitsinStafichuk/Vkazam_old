b5a117933e1e8d8c5bc49e6cb94c4a28a043fa59
sydym6 <sydym6@gmail.com>
2013-12-02 17:20:49 +0400

    Прикручен вк

--- src/com/git/programmerr47/testhflbjcrhjggkth/controllers/SettingsController.java
+++ src/com/git/programmerr47/testhflbjcrhjggkth/controllers/SettingsController.java
@@ -1,12 +1,20 @@
 package com.git.programmerr47.testhflbjcrhjggkth.controllers;
 
-import android.support.v4.app.FragmentManager;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 
 import com.git.programmerr47.testhflbjcrhjggkth.model.MicroScrobblerModel;
 import com.git.programmerr47.testhflbjcrhjggkth.model.RecognizeServiceConnection;
+import com.git.programmerr47.testhflbjcrhjggkth.utils.Constants;
 import com.git.programmerr47.testhflbjcrhjggkth.view.activities.SettingsActivity;
+import com.git.programmerr47.testhflbjcrhjggkth.view.activities.VkLoginActivity;
+import com.perm.kate.api.Api;
 
 public class SettingsController {
+	
+	private static final String TAG = "SettingsController";
+	private static final int REQUEST_VK_LOGIN = 1;
 
     private MicroScrobblerModel model;
     private SettingsActivity view;
@@ -16,11 +24,37 @@ public class SettingsController {
             this.model = RecognizeServiceConnection.getModel();
     }
 
-    public boolean hasLastfmAccount() {
-    	return false;
-    }
+    public boolean hasVkAccount() {
+    	Log.i(TAG, "model.getVkApi() == null: " + (model.getVkApi() == null));
+		return model.getVkApi() != null;
+	}
 
-    public void changeLastfmAccount(FragmentManager fm, String tag) {
-    }
+	public void changeVkAccount() {
+		if (model.getVkApi() == null) {
+			view.vkSignInOutButton.setEnabled(false);
+			Intent intent = new Intent();
+			intent.setClass(view, VkLoginActivity.class);
+			view.startActivityForResult(intent, REQUEST_VK_LOGIN);
+		} else {
+			model.setVkApi(null, 0, null);
+			view.changeVkButton();
+		}
+	}
+	
+	
+	public void checkOnActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_VK_LOGIN) {
+			if (resultCode == Activity.RESULT_OK) {
+				model.setVkApi(data.getStringExtra("token"), 
+						       data.getLongExtra("user_id", 0),
+						       new Api(data.getStringExtra("token"), Constants.VK_API_ID));
+				view.changeVkButton();
+				return;
+			}
+		}
+
+		model.setVkApi(null, 0, null);
+		view.changeVkButton();
+	}
 }
 
