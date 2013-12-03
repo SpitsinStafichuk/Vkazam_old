6517ad8cf695cad79fdc2b49ac1e9f5c380f123b
sydym6 <sydym6@gmail.com>
2013-12-03 19:30:59 +0400

    Просто переименованы фрагменты и адаптеры

---
+++ src/com/git/programmerr47/testhflbjcrhjggkth/view/fragments/SongReplacePPFragment.java
@@ -0,0 +1,33 @@
+package com.git.programmerr47.testhflbjcrhjggkth.view.fragments;
+
+import com.git.programmerr47.testhflbjcrhjggkth.R;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SongReplacePPFragment extends FragmentWithName{
+	
+	public static SongReplacePPFragment newInstance(String name, Context context, int resDraw) {
+		SongReplacePPFragment pageFragment = new SongReplacePPFragment();
+        Bundle arguments = new Bundle();
+        pageFragment.setArguments(arguments);
+        pageFragment.setFragmentName(name);
+        pageFragment.setContext(context);
+        pageFragment.setFragmentIcon(resDraw);
+        return pageFragment;
+    }
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+	
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_view_fragment, null);
+        return view;
+    }
+}
