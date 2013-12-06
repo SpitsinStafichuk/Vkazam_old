f96d460f6eda40c8257dbab2f068dd7a1b4f600f
Mikhail Spitsin <Mikhail.Spitsin@sidenis.com>
2013-12-06 09:36:22 +0400

    dynamic vk page

--- src/com/git/programmerr47/testhflbjcrhjggkth/view/adapters/SongReplacePagerAdapter.java
+++ src/com/git/programmerr47/testhflbjcrhjggkth/view/adapters/SongReplacePagerAdapter.java
@@ -4,14 +4,18 @@ import android.content.Context;
 import android.support.v4.app.FragmentManager;
 
 import com.git.programmerr47.testhflbjcrhjggkth.R;
+import com.git.programmerr47.testhflbjcrhjggkth.model.RecognizeServiceConnection;
 import com.git.programmerr47.testhflbjcrhjggkth.view.fragments.SongReplacePPFragment;
 import com.git.programmerr47.testhflbjcrhjggkth.view.fragments.SongReplaceVkFragment;
 
 public class SongReplacePagerAdapter extends PagerAdapter{
    
     public SongReplacePagerAdapter(FragmentManager fm, Context context) {
-            super(fm, 2);
-            fragments.add(SongReplacePPFragment.newInstance(context));
+        super(fm, 1);
+        fragments.add(SongReplacePPFragment.newInstance(context));
+        if (RecognizeServiceConnection.getModel().getVkApi() != null) {
+            setPageCount(2);
             fragments.add(SongReplaceVkFragment.newInstance(context));
+        }
     }
 }
