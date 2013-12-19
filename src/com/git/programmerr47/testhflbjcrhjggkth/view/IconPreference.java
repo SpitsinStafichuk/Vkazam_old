c81e01a29ae2102fc8c022e801d97d45591fd18d
Mikhail Spitsin <Mikhail.Spitsin@sidenis.com>
2013-12-19 11:04:16 +0400

    done timer, added base for settings timer delay

--- src/com/git/programmerr47/testhflbjcrhjggkth/view/IconPreference.java
+++ src/com/git/programmerr47/testhflbjcrhjggkth/view/IconPreference.java
@@ -1,36 +1,33 @@
 package com.git.programmerr47.testhflbjcrhjggkth.view;
 
+
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
-import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
 import com.git.programmerr47.testhflbjcrhjggkth.R;
 
-public class IconCheckBoxPreference extends CheckBoxPreference {
+public class IconPreference extends Preference {
 
     private Drawable mIcon;
     private View preference;
-    private View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            IconCheckBoxPreference.this.onClick();
-        }
-    };
+    private View.OnClickListener listener;
     private int iconVisibility;
 
-    public IconCheckBoxPreference(Context context, AttributeSet attrs) {
+    public IconPreference(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public IconCheckBoxPreference(Context context, AttributeSet attrs, int defStyle) {
+    public IconPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setLayoutResource(R.layout.icon_checkbox_preference);
+        setLayoutResource(R.layout.icon_preference);
         TypedArray a = context.obtainStyledAttributes(attrs,
-                R.styleable.IconCheckBoxPreference, defStyle, 0);
-        mIcon = a.getDrawable(R.styleable.IconCheckBoxPreference_iconCheckBox);
+                R.styleable.IconPreference, defStyle, 0);
+        mIcon = a.getDrawable(R.styleable.IconPreference_icon);
     }
 
     @Override
