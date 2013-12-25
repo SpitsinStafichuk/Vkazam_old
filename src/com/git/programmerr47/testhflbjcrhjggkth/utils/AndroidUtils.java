c44baa7781a6fea7221721dbafa4e5151d4b799a
Mikhail Spitsin <Mikhail.Spitsin@sidenis.com>
2013-12-25 17:44:18 +0400

    settings refactored

--- src/com/git/programmerr47/testhflbjcrhjggkth/utils/AndroidUtils.java
+++ src/com/git/programmerr47/testhflbjcrhjggkth/utils/AndroidUtils.java
@@ -2,7 +2,9 @@ package com.git.programmerr47.testhflbjcrhjggkth.utils;
 
 import android.content.Context;
 import android.os.Build;
+import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 
 public class AndroidUtils {
 
@@ -10,4 +12,15 @@ public class AndroidUtils {
         return Build.VERSION.SDK_INT <= Build.VERSION_CODES.HONEYCOMB_MR2 || (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB_MR2) && ViewConfiguration.get(context).hasPermanentMenuKey();
     }
 
+    public static void setViewEnabled(View view, boolean enabled) {
+        view.setEnabled(enabled);
+
+        if (view instanceof ViewGroup) {
+            ViewGroup group = (ViewGroup)view;
+
+            for ( int idx = 0 ; idx < group.getChildCount() ; idx++ ) {
+                setViewEnabled(group.getChildAt(idx), enabled);
+            }
+        }
+    }
 }
