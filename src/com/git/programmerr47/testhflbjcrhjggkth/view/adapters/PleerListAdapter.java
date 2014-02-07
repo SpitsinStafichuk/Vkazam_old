4466d0ac1f065b5050389e17cc9fef3d35ddce92
Mikhail Spitsin <Mikhail.Spitsin@sidenis.com>
2014-02-07 10:46:09 +0400

    code refactoring by adding URLlistAdapter

--- src/com/git/programmerr47/testhflbjcrhjggkth/view/adapters/PleerListAdapter.java
+++ src/com/git/programmerr47/testhflbjcrhjggkth/view/adapters/PleerListAdapter.java
@@ -21,37 +21,16 @@ import com.git.programmerr47.testhflbjcrhjggkth.model.pleer.api.KException;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PleerListAdapter extends BaseAdapter {
-
-    private MicroScrobblerModel model;
-    private DatabaseSongData currentSongData;
-    private View currentElement;
+public class PleerListAdapter extends URLlistAdapter {
     private List<Audio> urls;
-    private LayoutInflater inflater;
-    private FragmentActivity activity;
-    private int resLayout;
-    private int endOfListResLayout;
-    private int page = 1;
-    private ProgressBar newSongLoadingBar;
-    private boolean isFullList = false;
-    private boolean isAllSongWithOriginArtistShown = false;
-    private boolean isFirstClick = true;
-    private URLcontroller controller;
 
     public PleerListAdapter(final FragmentActivity activity, int resLayout, int endOfListResLayout) {
-        this.activity = activity;
-        this.resLayout = resLayout;
-        this.endOfListResLayout = endOfListResLayout;
-        controller = new URLcontroller(activity);
-        model = RecognizeServiceConnection.getModel();
-        currentSongData = model.getCurrentOpenSong();
-        Log.v("PleerListAdapter", "Current Song url = " + currentSongData.getPleercomUrl());
+        super(activity, resLayout, endOfListResLayout);
         urls = new ArrayList<Audio>();
-        updateSongsList();
-        inflater = (LayoutInflater) this.activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -68,182 +47,55 @@ public class PleerListAdapter extends BaseAdapter {
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    protected void addMoreUrls() throws KException, IOException, JSONException {
+        urls.addAll(Api.searchAudio(currentSongData.getArtist() + " " + currentSongData.getTitle(), 5, page));
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-
-        Log.v("PleerListAdapter", position + " - " + urls.size());
-        if (position < urls.size()) {
-            view = inflater.inflate(resLayout, parent, false);
-
-            TextView textView;
-
-            textView = (TextView) view.findViewById(R.id.ppUrlListItemArtist);
-            textView.setText(urls.get(position).artist);
-
-            textView = (TextView) view.findViewById(R.id.ppUrlListItemTitle);
-            textView.setText(urls.get(position).title);
-
-            textView = (TextView) view.findViewById(R.id.ppUrlListItemDuration);
-            textView.setText(getStringTime(urls.get(position).duration));
-
-            textView = (TextView) view.findViewById(R.id.ppUrlListItemBitRate);
-            textView.setText(urls.get(position).bitrate);
-
-            LinearLayout info = (LinearLayout) view.findViewById(R.id.ppUrlListItemInfo);
-            LinearLayout numbers = (LinearLayout) view.findViewById(R.id.ppUrlListItemNumbers);
-            RadioButton radioButton = (RadioButton) view.findViewById(R.id.ppUrlListItemCheckbutton);
-            View.OnClickListener listener = new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if ((currentSongData.getPleercomUrl() == null) || (!currentSongData.getPleercomUrl().equals(urls.get(position).url))) {
-                        currentSongData.setPleercomUrl(urls.get(position).url);
-                        currentSongData.setPpArtist(urls.get(position).artist);
-                        currentSongData.setPpTitle(urls.get(position).title);
-                        PleerListAdapter.this.notifyDataSetChanged();
-                    }
-                }
-            };
-            info.setOnClickListener(listener);
-            numbers.setOnClickListener(listener);
-            radioButton.setOnClickListener(listener);
-
-            if ((currentSongData.getPleercomUrl() != null) && (currentSongData.getPleercomUrl().equals(urls.get(position).url))) {
-                radioButton.setChecked(true);
-            } else {
-                radioButton.setChecked(false);
-            }
-        } else {
-            view = inflater.inflate(endOfListResLayout, parent, false);
-
-            newSongLoadingBar = (ProgressBar) view.findViewById(R.id.moreListItemLoading);
-            if (isFullList) {
-                view.setVisibility(View.GONE);
-            }
-
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    page++;
-                    updateSongsList();
-                }
-            });
-        }
-
-        if (position < urls.size()) {
-            ImageButton playPause = (ImageButton) view.findViewById(R.id.ppUrlListItemPlayPauseButton);
-            final View viewFinal = view;
-            playPause.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (isFirstClick) {
-                        model.getSongManager().release();
-                        isFirstClick = false;
-                    }
-
-                	Log.v("PleerListAdapter", "View = " + v);
-                    controller.setCurrentElement(viewFinal);
-                    SongData tempInfo = new SongData(null, urls.get(position).artist, null, urls.get(position).title, null);
-                    tempInfo.setPleercomUrl(urls.get(position).url);
-                    controller.playPauseSong(new DatabaseSongData(position, null, tempInfo), -1, SongManager.PP_SONG);
-                }
-            });
-
-            if (model.getSongManager().getSongData() != null) {
-                SongData data = model.getSongManager().getSongData();
-                if (data.getTrackId() == null) {
-                    if ((data.getPleercomUrl() != null) &&
-                        (data.getPleercomUrl().equals(urls.get(position).url)) &&
-                        (model.getSongManager().isPlaying())) {
-                        playPause.setImageResource(R.drawable.ic_media_pause);
-                    } else {
-                        playPause.setImageResource(R.drawable.ic_media_play);
-                    }
-                } else {
-                    playPause.setImageResource(R.drawable.ic_media_play);
-                }
-            } else {
-                playPause.setImageResource(R.drawable.ic_media_play);
-            }
-        }
-
-        return view;
+    protected String getArtist(int position) {
+        return urls.get(position).artist;
     }
 
-    private synchronized void updateSongsList() {
-        new Thread(new Runnable() {
+    @Override
+    protected String getTitle(int position) {
+        return urls.get(position).title;
+    }
+
+    @Override
+    protected long getDuration(int position) {
+        return urls.get(position).duration;
+    }
+
+    @Override
+    protected String getBitrate(int position) {
+        return urls.get(position).bitrate;
+    }
+
+    @Override
+    protected View.OnClickListener getOnViewClickListener(final int position) {
+        return new View.OnClickListener() {
             @Override
-            public void run() {
-
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        PleerListAdapter.this.newSongLoadingBar.setVisibility(View.VISIBLE);
-                    }
-                });
-                try {
-                    int listUpdate = urls.size();
-                    if (!isAllSongWithOriginArtistShown) {
-                        urls.addAll(Api.searchAudio(currentSongData.getArtist() + " " + currentSongData.getTitle(), 5, page));
-                    } else {
-                        if ((!currentSongData.getArtist().equals(currentSongData.getAlbumArtist())) &&
-                            (currentSongData.getAlbumArtist() != null)) {
-                            urls.addAll(Api.searchAudio(currentSongData.getAlbumArtist() + " " + currentSongData.getTitle(), 5, page));
-                        }
-                    }
-                    Log.v("PleerListAdapter", "ANSWER FROM INTERNET");
-                    final int listUpdateFinal = urls.size() - listUpdate;
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Log.v("PleerListAdapter", "" + listUpdateFinal);
-                            if (listUpdateFinal <= 0) {
-                                if (!isAllSongWithOriginArtistShown) {
-                                    isAllSongWithOriginArtistShown = true;
-                                } else {
-                                    isFullList = true;
-                                    Toast.makeText(activity, activity.getString(R.string.no_more_songs), Toast.LENGTH_SHORT).show();
-                                    Log.v("PleerListAdapter", "No more songs");
-                                }
-                            }
-                            PleerListAdapter.this.notifyDataSetChanged();
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } catch (JSONException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } catch (KException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } finally {
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            PleerListAdapter.this.newSongLoadingBar.setVisibility(View.GONE);
-                        }
-                    });
+            public void onClick(View view) {
+                if ((currentSongData.getPleercomUrl() == null) || (!currentSongData.getPleercomUrl().equals(urls.get(position).url))) {
+                    currentSongData.setPleercomUrl(urls.get(position).url);
+                    currentSongData.setPpArtist(urls.get(position).artist);
+                    currentSongData.setPpTitle(urls.get(position).title);
+                    PleerListAdapter.this.notifyDataSetChanged();
                 }
             }
-        }).start();
+        };
     }
 
-    private String getStringTime(long time) {
-        String result = "" + time / 60 + ":";
-        long seconds = time % 60;
-        if (seconds < 10) {
-            result += "0";
-        }
-        return result + seconds;
+    @Override
+    protected boolean isGetViewEqualsCurrentSong(int position, SongData data) {
+        return (data.getPleercomUrl() != null) &&
+                (data.getPleercomUrl().equals(urls.get(position).url));
     }
 
-    public void finish() {
-        if (!isFirstClick) {
-            model.getSongManager().release();
-            model.getSongManager().set(null, -1, model.getVkApi());
-        }
+    @Override
+    protected SongData generateSongDataByViewInfo(int position) {
+        SongData tempInfo = new SongData(null, urls.get(position).artist, null, urls.get(position).title, null);
+        tempInfo.setPleercomUrl(urls.get(position).url);
+        return tempInfo;
     }
 }
--- src/com/git/programmerr47/testhflbjcrhjggkth/view/adapters/VkListAdapter.java
+++ src/com/git/programmerr47/testhflbjcrhjggkth/view/adapters/VkListAdapter.java
@@ -22,40 +22,18 @@ import com.perm.kate.api.KException;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VkListAdapter extends BaseAdapter {
-	private static String TAG = "VkListAdapter";
-
-    private MicroScrobblerModel model;
-    private DatabaseSongData currentSongData;
-    private View currentElement;
+public class VkListAdapter extends URLlistAdapter {
     private List<Audio> audios;
-    private LayoutInflater inflater;
-    private FragmentActivity activity;
-    private int resLayout;
-    private int endOfListResLayout;
-    private int page = 1;
-    private ProgressBar newSongLoadingBar;
-    private boolean isFullList = false;
-    private boolean isAllSongWithOriginArtistShown = false;
-    private boolean isFirstClick = true;
-    private URLcontroller controller;
     private Api vkApi;
 
     public VkListAdapter(final FragmentActivity activity, int resLayout, int endOfListResLayout) {
-        this.activity = activity;
-        this.resLayout = resLayout;
-        this.endOfListResLayout = endOfListResLayout;
-        controller = new URLcontroller(activity);
-        model = RecognizeServiceConnection.getModel();
+        super(activity, resLayout, endOfListResLayout);
         vkApi = model.getVkApi();
-        currentSongData = model.getCurrentOpenSong();
-        //Log.v(TAG, "Current Song url = " + currentSongData.getPleercomUrl());
         audios = new ArrayList<Audio>();
-        updateSongsList();
-        inflater = (LayoutInflater) this.activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -72,188 +50,60 @@ public class VkListAdapter extends BaseAdapter {
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    protected void addMoreUrls() throws KException, IOException, JSONException {
+        audios.addAll(vkApi.searchAudio(currentSongData.getArtist() + " " + currentSongData.getTitle(), "2", "0", 5l, (page - 1) * 5l, null, null));
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-
-        Log.v(TAG, position + " - " + audios.size());
-        if (position < audios.size()) {
-            view = inflater.inflate(resLayout, parent, false);
-
-            TextView textView;
-
-            textView = (TextView) view.findViewById(R.id.ppUrlListItemArtist);
-            textView.setText(audios.get(position).artist);
-
-            textView = (TextView) view.findViewById(R.id.ppUrlListItemTitle);
-            textView.setText(audios.get(position).title);
-
-            textView = (TextView) view.findViewById(R.id.ppUrlListItemDuration);
-            textView.setText(getStringTime(audios.get(position).duration));
-
-            textView = (TextView) view.findViewById(R.id.ppUrlListItemBitRate);
-
-            LinearLayout info = (LinearLayout) view.findViewById(R.id.ppUrlListItemInfo);
-            LinearLayout numbers = (LinearLayout) view.findViewById(R.id.ppUrlListItemNumbers);
-            RadioButton radioButton = (RadioButton) view.findViewById(R.id.ppUrlListItemCheckbutton);
-            
-            View.OnClickListener listener = new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                	Audio audio = audios.get(position);
-                	String audioId = audio.owner_id + "_" + audio.aid;
-                	long lyricsId = -1;
-                	if (audio.lyrics_id != null) {
-                		lyricsId = audio.lyrics_id;
-                	}
-                    if (currentSongData.getVkAudioId() == null || !currentSongData.getVkAudioId().equals(audioId)) {
-                        currentSongData.setVkAudioId(audioId);
-                        currentSongData.setLyricsId(lyricsId);
-                        VkListAdapter.this.notifyDataSetChanged();
-                    }
-                }
-            };
-            info.setOnClickListener(listener);
-            numbers.setOnClickListener(listener);
-            radioButton.setOnClickListener(listener);
-            
-            String audioId = audios.get(position).owner_id + "_" + audios.get(position).aid;
-            if ((currentSongData.getVkAudioId() != null) && (currentSongData.getVkAudioId().equals(audioId))) {
-                radioButton.setChecked(true);
-            } else {
-                radioButton.setChecked(false);
-            }
-        } else {
-            view = inflater.inflate(endOfListResLayout, parent, false);
-
-            newSongLoadingBar = (ProgressBar) view.findViewById(R.id.moreListItemLoading);
-            if (isFullList) {
-                view.setVisibility(View.GONE);
-            }
-
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    page++;
-                    updateSongsList();
-                }
-            });
-        }
-
-        if (position < audios.size()) {
-            ImageButton playPause = (ImageButton) view.findViewById(R.id.ppUrlListItemPlayPauseButton);
-            final View viewFinal = view;
-            playPause.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (isFirstClick) {
-                        model.getSongManager().release();
-                        isFirstClick = false;
-                    }
-
-                	Log.v(TAG, "View = " + v);
-                    controller.setCurrentElement(viewFinal);
-                    SongData tempInfo = new SongData(null, audios.get(position).artist, null, audios.get(position).title, null);
-                    tempInfo.setVkAudioId(audios.get(position).owner_id + "_" + audios.get(position).aid);
-                    controller.playPauseSong(new DatabaseSongData(position, null, tempInfo), -1, SongManager.VK_SONG);
-                }
-            });
-
-            if (model.getSongManager().getSongData() != null) {
-                SongData data = model.getSongManager().getSongData();
-                if (data.getTrackId() == null) {
-                    if ((data.getVkAudioId() != null) &&
-                            (data.getVkAudioId().equals(audios.get(position).owner_id + "_" + audios.get(position).aid)) &&
-                            (model.getSongManager().isPlaying())) {
-                        playPause.setImageResource(R.drawable.ic_media_pause);
-                    } else {
-                        playPause.setImageResource(R.drawable.ic_media_play);
-                    }
-                } else {
-                    playPause.setImageResource(R.drawable.ic_media_play);
-                }
-            } else {
-                playPause.setImageResource(R.drawable.ic_media_play);
-            }
-        }
-
-        return view;
+    protected String getArtist(int position) {
+        return audios.get(position).artist;
     }
 
-    private synchronized void updateSongsList() {
-        new Thread(new Runnable() {
+    @Override
+    protected String getTitle(int position) {
+        return audios.get(position).title;
+    }
+
+    @Override
+    protected long getDuration(int position) {
+        return audios.get(position).duration;
+    }
+
+    @Override
+    protected String getBitrate(int position) {
+        return null;
+    }
+
+    @Override
+    protected View.OnClickListener getOnViewClickListener(final int position) {
+        return new View.OnClickListener() {
             @Override
-            public void run() {
-
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        VkListAdapter.this.newSongLoadingBar.setVisibility(View.VISIBLE);
-                    }
-                });
-                try {
-                    int listUpdate = audios.size();
-                    if (!isAllSongWithOriginArtistShown) {
-                        audios.addAll(vkApi.searchAudio(currentSongData.getArtist() + " " + currentSongData.getTitle(), "2", "0", 5l, (page - 1) * 5l, null, null));
-                    } else {
-                        if ((!currentSongData.getArtist().equals(currentSongData.getAlbumArtist())) &&
-                                (currentSongData.getAlbumArtist() != null)) {
-                            audios.addAll(vkApi.searchAudio(currentSongData.getAlbumArtist() + " " + currentSongData.getTitle(), "2", "0", 5l, (page - 1) * 5l, null, null));
-                        }
-                    }
-                    Log.v(TAG, "ANSWER FROM INTERNET");
-                    final int listUpdateFinal = audios.size() - listUpdate;
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Log.v(TAG, "" + listUpdateFinal);
-                            if (listUpdateFinal <= 0) {
-                                if (!isAllSongWithOriginArtistShown) {
-                                    isAllSongWithOriginArtistShown = true;
-                                } else {
-                                    isFullList = true;
-                                    Toast.makeText(activity, activity.getString(R.string.no_more_songs), Toast.LENGTH_SHORT).show();
-                                    Log.v(TAG, "No more songs");
-                                }
-                            }
-                            VkListAdapter.this.notifyDataSetChanged();
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } catch (JSONException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } catch (KException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } finally {
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            VkListAdapter.this.newSongLoadingBar.setVisibility(View.GONE);
-                        }
-                    });
+            public void onClick(View view) {
+                Audio audio = audios.get(position);
+                String audioId = audio.owner_id + "_" + audio.aid;
+                long lyricsId = -1;
+                if (audio.lyrics_id != null) {
+                    lyricsId = audio.lyrics_id;
+                }
+                if (currentSongData.getVkAudioId() == null || !currentSongData.getVkAudioId().equals(audioId)) {
+                    currentSongData.setVkAudioId(audioId);
+                    currentSongData.setLyricsId(lyricsId);
+                    notifyDataSetChanged();
                 }
             }
-        }).start();
+        };
     }
 
-    private String getStringTime(long time) {
-        String result = "" + time / 60 + ":";
-        long seconds = time % 60;
-        if (seconds < 10) {
-            result += "0";
-        }
-        return result + seconds;
+    @Override
+    protected boolean isGetViewEqualsCurrentSong(int position, SongData data) {
+        return (data.getVkAudioId() != null) &&
+                (data.getVkAudioId().equals(audios.get(position).owner_id + "_" + audios.get(position).aid));
     }
 
-    public void finish() {
-        if (!isFirstClick) {
-            model.getSongManager().release();
-            model.getSongManager().set(null, -1, model.getVkApi());
-        }
+    @Override
+    protected SongData generateSongDataByViewInfo(int position) {
+        SongData tempInfo = new SongData(null, audios.get(position).artist, null, audios.get(position).title, null);
+        tempInfo.setVkAudioId(audios.get(position).owner_id + "_" + audios.get(position).aid);
+        return tempInfo;
     }
 }
