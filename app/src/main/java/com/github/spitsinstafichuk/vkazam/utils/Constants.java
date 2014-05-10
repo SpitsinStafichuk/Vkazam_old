56cebed30d2d8a606a56e26da9d2134fed71a82f
sydym6 <sydym6@gmail.com>
2014-05-10 13:51:03 +0400

    move to gradle

---
+++ app/src/main/java/com/github/spitsinstafichuk/vkazam/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.github.spitsinstafichuk.vkazam.utils;
+
+/**
+ *
+ */
+public interface Constants {
+    String VK_API_ID = "4104000";
+    String GRACENOTE_APPLICATION_ID = "2826240-6B775924534E29CFFB17A317306BD5C6";
+}
